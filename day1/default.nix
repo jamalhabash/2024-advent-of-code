fe8c4cfe4b4c45e305448b0f73052070a86c5d6b
Jamal Habash <git@jamal.haba.sh>
2024-12-01 23:23:18 -0500

    result 1

--- day1/default.nix
+++ day1/default.nix
@@ -9,11 +9,18 @@ let
     in
     lib.toInt (lib.elemAt numbers element);
 
+  abs = x: if x < 0 then -x else x;
+
 in
 
 rec {
   parsed_left = map (parser 0) input;
   parsed_right = map (parser 3) input;
-  sorted_parsed_left = lib.lists.sort (a: b: a<b) parsed_left;
-  sorted_parsed_right = lib.lists.sort (a: b: a>b) parsed_right;
+  sorted_parsed_left = lib.lists.sort (a: b: a < b) parsed_left;
+  sorted_parsed_right = lib.lists.sort (a: b: a < b) parsed_right;
+  distances_list = map abs (
+    lib.lists.zipListsWith (a: b: a - b) sorted_parsed_left sorted_parsed_right
+  );
+  #  test = map abs new_list;
+  result = lib.lists.foldl (a: b: a + b) 0 distances_list;
 }
