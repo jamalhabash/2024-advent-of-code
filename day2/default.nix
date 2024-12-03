3a33448ad5a0274e2954652e2a972a439cb1e182
Jamal Habash <git@jamal.haba.sh>
2024-12-02 22:03:30 -0500

    day 2 part 2

--- day2/default.nix
+++ day2/default.nix
@@ -9,10 +9,6 @@ let
     in
     lib.forEach numbers (x: lib.toInt x);
 
-  # goesUp = list: lib.foldl (a: b: a < b) (builtins.head list) (builtins.tail list);
-  # goesUp = list: lib.foldl (a: b: a && (b < b)) true (builtins.tail list);
-  # goesUp = list: lib.foldl' (a: b: a && b > a) true (builtins.tail list);
-
   parsed_input = map parser input;
 
   checkAscendingPair = a: b: a <= b && (b - a <= 3 && b - a >= 1);
@@ -46,7 +42,30 @@ let
 
   result = lib.lists.foldl (a: b: if b then a + 1 else a) 0 step1;
 
+  remove_one_element =
+    list:
+    let
+      len = builtins.length list;
+      indices = builtins.genList (x: x) len;
+      removeAt =
+        i: list:
+        let
+          before = lib.sublist 0 i list;
+          after = lib.sublist (i + 1) (len - i - 1) list;
+        in
+        before ++ after;
+    in
+    [ list ] ++ map (i: removeAt i list) indices;
+
+  any = list: if list == [ ] then false else (builtins.head list) || (any (builtins.tail list));
+
+  lists_with_removed_elements = lib.forEach parsed_input (x: remove_one_element x);
+  checked_lists = lib.forEach lists_with_removed_elements (
+    x: lib.forEach x (x: isAscending x || isDescending x)
+  );
+  z = lib.forEach checked_lists (x: any x);
 in
 rec {
   inherit result;
+  result2 = lib.lists.foldl (a: b: if b then a + 1 else a) 0 z;
 }
