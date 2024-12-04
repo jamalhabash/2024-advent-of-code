0377988ce76e37f4669e21de27427e661bee3365
Jamal Habash <git@jamal.haba.sh>
2024-12-04 14:00:04 -0500

    part one and part two

--- day3/default.nix
+++ day3/default.nix
@@ -1,54 +1,92 @@
 with import <nixpkgs> { };
 let
 
+  #inputs
+  input = lib.fileContents ./input;
+
+  #regex
+  number_pair_regex = "([0-9]{1,3},[0-9]{1,3})";
   mul_regex = "(mul[(][0-9]{1,3},[0-9]{1,3}[)])";
   do_regex = "(do[(][)])";
   dont_regex = "(don[']t[(][)])";
-  do_dont_regex = do_regex + "|" dont_regex;
-  parse_do = x: builtins.split "(do[(][)])|(don[']t[(][)])" x;
-  match_mul = x: builtins.match "(mul[(][0-9]{1,3},[0-9]{1,3}[)])" x;
+  do_dont_regex = do_regex + "|" + dont_regex;
 
-  input = lib.fileContents ./input2;
+  #split and match functions
+  split_do_dont = x: builtins.split do_dont_regex x;
+  split_mul = x: builtins.split mul_regex x;
+  match_mul = x: builtins.match mul_regex x;
+  match_do_dont = x: builtins.match do_dont_regex x;
 
-  parser1 = x: builtins.split "(mul[(][0-9]{1,3},[0-9]{1,3}[)])" x;
+  process_mul =
+    x:
+    let
+      component_list = lib.flatten (builtins.split "([0-9]{1,3})" x);
+      first_number = lib.toInt (lib.elemAt component_list 1);
+      second_number = lib.toInt (lib.elemAt component_list 3);
+      output = first_number * second_number;
+    in
+    output;
 
-  result = lib.forEach (parser1 input) (x: if (lib.isString x) then parse_do x else x);
-
-  remove_strings = x: lib.forEach x (x: if (lib.isString x) then null else x);
-  result2 = remove_strings result;
-  remove = lib.forEach result2 (
-    x: lib.forEach x (a: if (lib.isString a && (match_mul a) == null) then null else a)
+  list_with_extracted_mul_do_dont = lib.remove null (
+    #last step, remove all the nulls and you're left with the muls, dos and donts
+    lib.flatten (
+      #flatten all the sublists that have been created
+      lib.forEach (split_mul input) (
+        #this for each is operating on a list with the muls already pulled out. This code block is to extract the dos and donts
+        x:
+        if (lib.isString x) then
+          let
+            split_list = split_do_dont x;
+            extracted_do_dont = if builtins.length split_list > 1 then lib.elemAt split_list 1 else null;
+          in
+          extracted_do_dont
+        else
+          x
+      )
+    )
   );
-  remove2 = lib.remove null (lib.flatten remove);
-  remove3 = lib.forEach remove2 (
-    x: if (match_mul x) == null then x else builtins.split "([0-9]{1,3},[0-9]{1,3})" x
+
+  result_part_one = lib.foldl (a: b: if builtins.isInt b then a + b else a + 0) 0 (
+    lib.forEach list_with_extracted_mul_do_dont (
+      x: if (builtins.match mul_regex x != null) then process_mul x else x
+    )
   );
+
+  # if it's an integer, check if the current command is do and add to the sum, else return the previous result
+  # if it's not an integer, replace the current command with this new one
+
+  result_part_two =
+    lib.foldl
+      (
+        a: b:
+        if builtins.isInt b then
+          if a.command == "do()" then
+            {
+              command = a.command;
+              sum = a.sum + b;
+            }
+          else
+            a
+        else
+          {
+            command = b;
+            sum = a.sum;
+          }
+
+      )
+      {
+        #initial conditions
+        command = "do()";
+        sum = 0;
+      }
+      (
+        lib.forEach list_with_extracted_mul_do_dont (
+          x: if (builtins.match mul_regex x != null) then process_mul x else x
+        )
+      );
 
 in
-# parser =
-#   line:
-#   let
-#     # numbers = builtins.match "/mul\([1-9]{1,3},[1-9]{1,3}\)/" line;
-#     # numbers = builtins.match "mul" line;
-#     # numbers = builtins.match "/.*mul\([1-9]{1,3},[1-9]{1,3}\)*./" line;
-#     # numbers = builtins.match "mul" line;
-#     numbers = x: builtins.match "[0-9]{1,3},[0-9]{1,3}" x;
-#     parsed_line = lib.splitString "mul(" line;
-#     parsed_line2 = lib.forEach parsed_line (x: lib.splitString ")" x);
-#     parsed_line3 = lib.forEach parsed_line2 (x: lib.elemAt x 0);
-#     parsed_line4 = lib.forEach parsed_line3 (x: if ((numbers x) != null) then x else 0);
-#     parsed_line5 = lib.remove 0 parsed_line4;
-#     parsed_line6 = lib.forEach parsed_line5 (x: lib.forEach (lib.splitString "," x) (y: lib.toInt y));
-#     parsed_line7 = lib.forEach parsed_line6 (x: lib.foldl (a: b: a * b) 1 x);
-#     parsed_line8 = lib.foldl (a: b: a + b) 0 parsed_line7;
-#   in
-#   parsed_line8;
-#
-# parsed_input = map parser input;
-# result = lib.foldl (a: b: a + b) 0 parsed_input;
 {
-  # inherit parsed_input;
-  inherit remove3;
-  #test = number "mul(3,4)";
-  #test2 = number "5";
+  inherit result_part_one;
+  inherit result_part_two;
 }
